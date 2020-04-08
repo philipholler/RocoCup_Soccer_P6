2ac16cd9cc5c89a2e88366155aecdae0d1632edc
philipholler <pholle17@student.aau.dk>
2020-04-08 13:28:22 +0200

    Uppaal model can now change arguments for system declarations.

--- src/uppaal/verifyta.py
+++ src/uppaal/verifyta.py
@@ -5,28 +5,28 @@ from pathlib import Path
 
 from player.player import WorldView
 
-VERIFYTA_PATH = Path(__file__).parent / 'bin' / 'verifyta'
-VERIFYTA_QUERIES_PATH = Path(__file__).parent.parent / 'verifyta' / 'queries'
-VERIFYTA_MODELS_PATH = Path(__file__).parent.parent / 'verifyta' / 'models'
-VERIFYTA_OUTPUT_DIR_PATH = Path(__file__).parent.parent / 'verifyta' / 'outputdir'
+VERIFYTA_PATH = Path(__file__).parent / 'bin' / 'uppaal'
+VERIFYTA_QUERIES_PATH = Path(__file__).parent.parent / 'uppaal' / 'queries'
+VERIFYTA_MODELS_PATH = Path(__file__).parent.parent / 'uppaal' / 'models'
+VERIFYTA_OUTPUT_DIR_PATH = Path(__file__).parent.parent / 'uppaal' / 'outputdir'
 
 
 def generate_strategy(wv: WorldView, xml_file_name: str, queries_file_name: str):
     update_xml_file(wv)
     # 1. Update XML file
-    # 2. Run verifyta
+    # 2. Run uppaal
     # 3. Input strategy to coach
 
-    # Prepared for using verifyta
+    # Prepared for using uppaal
     # verifyta_path --print-strategies outputdir xml_path queries_dir learning-method?
     command = "{0} --print-strategies {1} {2} {3}".format(VERIFYTA_PATH, VERIFYTA_OUTPUT_DIR_PATH
                                                           , VERIFYTA_MODELS_PATH / xml_file_name
                                                           , VERIFYTA_QUERIES_PATH / queries_file_name)
 
-    # Run verifyta with the arguments given
+    # Run uppaal with the arguments given
     verifyta = subprocess.Popen(command, shell=True)
 
-    # Wait for verifyta to finish generating and printing strategy
+    # Wait for uppaal to finish generating and printing strategy
     while verifyta.poll() is None:
         time.sleep(0.001)
 
