dc32e6fd6b27955a91e8499dcb09b334c5a36e39
Magnus <magnjensen@gmail.com>
2020-04-14 13:49:04 +0200

    Added function for finding closest opponents given a list of team players.

--- src/coach/world_objects_coach.py
+++ src/coach/world_objects_coach.py
@@ -5,7 +5,7 @@ class PlayerViewCoach:
     def __init__(self, team, num, is_goalie, coord, delta_x, delta_y, body_angle, neck_angle, has_ball) -> None:
         super().__init__()
         self.team = team
-        self.num = num
+        self.num = int(num)
         self.is_goalie = is_goalie
         self.coord: Coordinate = coord
         self.has_ball: bool = has_ball
@@ -17,8 +17,8 @@ class PlayerViewCoach:
     def __repr__(self) -> str:
         return "(team: {0}, num: {1}, is_goalie: {2}, coord: {3}, delta_x: {4}, delta_y: {5}, body_angle: {6}, " \
                "neck_angle: {7}, has_ball: {8})".format(self.team, self.num, self.is_goalie, self.coord
-                                         , self.delta_x, self.delta_y, self.body_angle
-                                         , self.neck_angle, self.has_ball)
+                                                        , self.delta_x, self.delta_y, self.body_angle
+                                                        , self.neck_angle, self.has_ball)
 
 
 def distance_to_ball(play: PlayerViewCoach, ball: Coordinate):
@@ -41,11 +41,25 @@ class WorldViewCoach:
 
     def get_closest_team_players_to_ball(self, amount) -> [PlayerViewCoach]:
         # Get only players from same team
-        same_team_players: [PlayerViewCoach] = filter(lambda x: (x.team == self.team), self.players)
+        same_team_players: [PlayerViewCoach] = list(filter(lambda x: (x.team == self.team), self.players))
         # Sort by distance to ball
-        sorted_list: [PlayerViewCoach] = sorted(same_team_players, key=lambda play: distance_to_ball(play, self.ball.coord))
+        sorted_list: [PlayerViewCoach] = sorted(same_team_players,
+                                                key=lambda play: distance_to_ball(play, self.ball.coord))
         # Return only the first *amount* of players
         return sorted_list[:amount]
+
+    def get_closest_opponents(self, team_players: [PlayerViewCoach], amount: int) -> [PlayerViewCoach]:
+        opposing_team_players: [PlayerViewCoach] = list(filter(lambda x: (x.team != self.team), self.players))
+        closest_opponents = []
+        for team_member in team_players:
+            opponents_sorted: [PlayerViewCoach] = sorted(opposing_team_players,
+                                                         key=lambda opponent: opponent.coord.euclidean_distance_from(
+                                                             team_member.coord))
+            closest = opponents_sorted[0]
+            opposing_team_players.remove(closest)
+            closest_opponents.append(closest)
+
+        return closest_opponents[:amount]
 
     def get_closest_players_to_ball(self, amount) -> [PlayerViewCoach]:
         # Sort by distance to ball
@@ -63,6 +77,3 @@ class BallOnlineCoach:
 
     def __repr__(self) -> str:
         return "(Coord: {0}, delta_x: {1}, delta_y: {2})".format(self.coord, self.delta_x, self.delta_y)
-
-
-
--- tests/test_world_objects_coach.py
+++ tests/test_world_objects_coach.py
@@ -3,12 +3,13 @@ from unittest import TestCase
 from coach.world_objects_coach import WorldViewCoach, PlayerViewCoach, BallOnlineCoach
 from player.world_objects import Coordinate
 
+
 class TestWorldViewCoach(TestCase):
     def test_get_closest_team_players_to_ball_00(self):
         wv = WorldViewCoach(0, "Team1")
-        player1 = PlayerViewCoach("Team1", "1", False, Coordinate(10, 10), 0, 0, 0, 0)
-        player2 = PlayerViewCoach("Team1", "2", False, Coordinate(5, 5), 0, 0, 0, 0)
-        player3 = PlayerViewCoach("Team1", "3", False, Coordinate(0, 0), 0, 0, 0, 0)
+        player1 = PlayerViewCoach("Team1", 1, False, Coordinate(10, 10), 0, 0, 0, 0, False)
+        player2 = PlayerViewCoach("Team1", 2, False, Coordinate(5, 5), 0, 0, 0, 0, False)
+        player3 = PlayerViewCoach("Team1", 3, False, Coordinate(0, 0), 0, 0, 0, 0, False)
         wv.players.append(player1)
         wv.players.append(player3)
         wv.players.append(player2)
@@ -16,15 +17,15 @@ class TestWorldViewCoach(TestCase):
 
         result: [PlayerViewCoach] = wv.get_closest_team_players_to_ball(3)
         self.assertEqual(len(result), 3, "Amount of results should fit the argument.")
-        self.assertEqual(result[0].num, "3", "Player 3 should be the closest")
-        self.assertEqual(result[1].num, "2", "Player 2 should be second closest")
-        self.assertEqual(result[2].num, "1", "Player 1 should be furthest away")
+        self.assertEqual(result[0].num, 3, "Player 3 should be the closest")
+        self.assertEqual(result[1].num, 2, "Player 2 should be second closest")
+        self.assertEqual(result[2].num, 1, "Player 1 should be furthest away")
 
     def test_get_closest_team_players_to_ball_01(self):
         wv = WorldViewCoach(0, "Team1")
-        player1 = PlayerViewCoach("Team1", "1", False, Coordinate(10, 10), 0, 0, 0, 0)
-        player2 = PlayerViewCoach("Team1", "2", False, Coordinate(5, 5), 0, 0, 0, 0)
-        player3 = PlayerViewCoach("Team1", "3", False, Coordinate(100, 100), 0, 0, 0, 0)
+        player1 = PlayerViewCoach("Team1", 1, False, Coordinate(10, 10), 0, 0, 0, 0, False)
+        player2 = PlayerViewCoach("Team1", 2, False, Coordinate(5, 5), 0, 0, 0, 0, False)
+        player3 = PlayerViewCoach("Team1", 3, False, Coordinate(100, 100), 0, 0, 0, 0, False)
         wv.players.append(player1)
         wv.players.append(player3)
         wv.players.append(player2)
@@ -33,15 +34,14 @@ class TestWorldViewCoach(TestCase):
         # take only 2 closest players
         result: [PlayerViewCoach] = wv.get_closest_team_players_to_ball(2)
         self.assertEqual(len(result), 2, "Amount of results should fit the argument.")
-        self.assertEqual(result[0].num, "2", "Player 2 should be the closest")
-        self.assertEqual(result[1].num, "1", "Player 1 should be second closest")
-
+        self.assertEqual(result[0].num, 2, "Player 2 should be the closest")
+        self.assertEqual(result[1].num, 1, "Player 1 should be second closest")
 
     def test_get_closest_team_players_to_ball_02(self):
         wv = WorldViewCoach(0, "Team1")
-        player1 = PlayerViewCoach("Team1", "1", False, Coordinate(10, 10), 0, 0, 0, 0)
-        player2 = PlayerViewCoach("Team1", "2", False, Coordinate(5, 5), 0, 0, 0, 0)
-        player3 = PlayerViewCoach("Team2", "3", False, Coordinate(100, 100), 0, 0, 0, 0)
+        player1 = PlayerViewCoach("Team1", 1, False, Coordinate(10, 10), 0, 0, 0, 0, False)
+        player2 = PlayerViewCoach("Team1", 2, False, Coordinate(5, 5), 0, 0, 0, 0, False)
+        player3 = PlayerViewCoach("Team2", 3, False, Coordinate(100, 100), 0, 0, 0, 0, False)
 
         wv.players.append(player1)
         wv.players.append(player3)
@@ -52,14 +52,14 @@ class TestWorldViewCoach(TestCase):
         result: [PlayerViewCoach] = wv.get_closest_team_players_to_ball(2)
         # Player 3 is team 2, and should not be included
         self.assertEqual(len(result), 2, "Amount of results should fit the argument")
-        self.assertEqual(result[0].num, "2", "Player 2 should be the closest")
-        self.assertEqual(result[1].num, "1", "Player 1 should be second closest")
+        self.assertEqual(result[0].num, 2, "Player 2 should be the closest")
+        self.assertEqual(result[1].num, 1, "Player 1 should be second closest")
 
     def test_get_closest_team_players_to_ball_03(self):
         wv = WorldViewCoach(0, "Team2")
-        player1 = PlayerViewCoach("Team1", "1", False, Coordinate(10, 10), 0, 0, 0, 0)
-        player2 = PlayerViewCoach("Team1", "2", False, Coordinate(5, 5), 0, 0, 0, 0)
-        player3 = PlayerViewCoach("Team2", "3", False, Coordinate(100, 100), 0, 0, 0, 0)
+        player1 = PlayerViewCoach("Team1", 1, False, Coordinate(10, 10), 0, 0, 0, 0, False)
+        player2 = PlayerViewCoach("Team1", 2, False, Coordinate(5, 5), 0, 0, 0, 0, False)
+        player3 = PlayerViewCoach("Team2", 3, False, Coordinate(100, 100), 0, 0, 0, 0, False)
 
         wv.players.append(player1)
         wv.players.append(player3)
@@ -70,4 +70,35 @@ class TestWorldViewCoach(TestCase):
         result: [PlayerViewCoach] = wv.get_closest_team_players_to_ball(1)
         # Player 3 is team 2, the rest should not be included
         self.assertEqual(len(result), 1, "Amount of results should fit the argument")
-        self.assertEqual(result[0].num, "3", "Player 3 should be the closest")
+        self.assertEqual(result[0].num, 3, "Player 3 should be the closest")
+
+    def test_get_closest_opponents(self):
+        wv = WorldViewCoach(0, "Team1")
+        wv.ball = BallOnlineCoach(Coordinate(0, 0), 0, 0)
+        for team in ["Team1", "Team2"]:
+            for num in range(0, 11):
+                posx = (20 - num) if team == "Team1" else num
+                wv.players.append(PlayerViewCoach(team, str(num), False, Coordinate(posx, posx), 0, 0, 0, 0, False))
+
+        closest_team_members = wv.get_closest_team_players_to_ball(5)
+        closest_opponents = wv.get_closest_opponents(closest_team_members, 5)
+
+        for i in range(0, 5):
+            self.assertEqual(10 - i, closest_opponents[i].num)
+
+    def test_get_closest_ordered_opponents(self):
+        wv = WorldViewCoach(0, "Team1")
+        wv.ball = BallOnlineCoach(Coordinate(0, 0), 0, 0)
+        for team in ["Team1", "Team2"]:
+            for num in range(0, 11):
+                wv.players.append(
+                    PlayerViewCoach(team, str(num), False, Coordinate(num, num), 0, 0, 0, 0, False))
+
+        closest_team_members = wv.get_closest_team_players_to_ball(5)
+        closest_opponents = wv.get_closest_opponents(closest_team_members, 5)
+
+        for i in range(0, 5):
+            self.assertEqual(i, closest_opponents[i].num)
+
+
+
