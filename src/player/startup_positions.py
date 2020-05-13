69d5e139e753a2b608214f9fb0e21732998211ce
Magnus <magnjensen@gmail.com>
2020-05-13 15:11:59 +0200

    Fixed a bug that caused indented brackets to not count as closing brackets for function definitions in uppaal model files.

--- src/player/startup_positions.py
+++ src/player/startup_positions.py
@@ -1,5 +1,5 @@
 # x_pos, y_pos, objective behaviour
-goalie_pos = [(-50, 0, "default")]
-defenders_pos = [(-36, 20, "default"), (-36, 7, "default"), (-36, -7, "default"), (-36, -20, "default")]
-midfielders_pos = [(-20, 7, "default"), (-20, -7, "default")]
-strikers_pos = [(-3, 9, "default"), (-3, -9, "default"), (-0.5, 25, "default"), (-0.5, -25, "default")]
+goalie_pos = [(-50, 0, "biptest")]
+defenders_pos = [(-36, 20, "biptest"), (-36, 7, "biptest"), (-36, -7, "biptest"), (-36, -20, "biptest")]
+midfielders_pos = [(-20, 7, "biptest"), (-20, -7, "biptest")]
+strikers_pos = [(-3, 9, "biptest"), (-3, -9, "biptest"), (-0.5, 25, "biptest"), (-0.5, -25, "biptest")]
